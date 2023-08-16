68e9a14e5a8318eab80179f73385a126b64d694d
wernermona96 <wernermona@icloud.com>
2023-08-16 20:11:39 +0200

    almost finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
-import useCurrentUser from "@/hooks/usecurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import InfoModal from "@/components/InfoModal";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,13 +27,22 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-export default function Home() {
-  const { data: user } = useCurrentUser();
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModalStore();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Les + vus" data={movies} />
+        <MovieList title="Mes favorits" data={favorites} />
+      </div>
     </>
   );
-}
+};
+
+export default Home;
