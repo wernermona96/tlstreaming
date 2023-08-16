da454fdcddf506d06f3973096fb21646d7ee6412
wernermona96 <wernermona@icloud.com>
2023-08-16 17:01:37 +0200

    home page whit video turning

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -4,7 +4,18 @@ import useBillboard from "@/hooks/useBillboard";
 const Billboard: React.FC = () => {
   const { data } = useBillboard();
 
-  return <div></div>;
+  return (
+    <div className="relative h-[56.25vw]">
+      <video
+        poster={data?.thumbnailUrl}
+        className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500"
+        autoPlay
+        muted
+        loop
+        src={data?.videoUrl}
+      ></video>
+    </div>
+  );
 };
 
 export default Billboard;
